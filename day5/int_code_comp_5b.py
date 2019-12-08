ca9adf972f58b7871ad6d15546493ed7419956d6
Erik Kristofer Anderson <erik.kristofer.anderson@gmail.com>
2019-12-08 15:56:50 -0600

    create int_code_comp_5b for day 5 part 2
    It starts as a copy of int_code_comp_5a

---
+++ day5/int_code_comp_5b.py
@@ -0,0 +1,69 @@
+def compute(tape, user_input):
+    program_input = user_input
+    program_output = None
+    i = 0
+    while True:
+        instruction = str(tape[i])
+        opcode = int(instruction[-2:])
+        # check for halt code
+        if opcode == 99:
+            return program_output
+            break
+
+        # read in modes
+        modes = [0, 0, 0]
+        try:
+            modes[0] = int(instruction[-3])
+            pass
+        except IndexError:
+            modes[0] = 0
+        try:
+            modes[1] = int(instruction[-4])
+        except IndexError:
+            modes[1] = 0
+        try:
+            modes[2] = int(instruction[-5])
+        except IndexError:
+            modes[2] = 0
+
+        # handle opcode 1
+        # opcode = 1
+        if opcode == 1:
+            a = tape[tape[i + 1]] if modes[0] == 0 else tape[i + 1]
+            b = tape[tape[i + 2]] if modes[1] == 0 else tape[i + 2]
+            c = a + b
+            if modes[2] == 0:
+                tape[tape[i+3]] = c
+            else:
+                tape[i+3] = c
+            i += 4
+
+        # handle opcode 2 (Should be identical to opcode 1 except that it multiplies
+        if opcode == 2:
+            a = tape[tape[i + 1]] if modes[0] == 0 else tape[i + 1]
+            b = tape[tape[i + 2]] if modes[1] == 0 else tape[i + 2]
+            c = a * b
+            if modes[2] == 0:
+                tape[tape[i + 3]] = c
+            else:
+                tape[i + 3] = c
+            i += 4
+
+        # handle opcode 3
+        # todo what is input and output for this?
+        if opcode == 3:
+            if modes[0] == 0:
+                tape[tape[i+1]] = program_input
+            else:
+                tape[i+1] = program_input
+            i += 2
+
+        if opcode == 4:
+            if modes[0] == 0:
+                program_output = tape[tape[i+1]]
+            else:
+                program_output = tape[i+1]
+            i += 2
+
+        if program_output:
+            program_input = program_output
