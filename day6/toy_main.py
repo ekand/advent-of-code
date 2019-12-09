6f0ea8d4c28bca5804b3ca9b47dd864e4cf561fa
Erik Kristofer Anderson <erik.kristofer.anderson@gmail.com>
2019-12-08 22:05:14 -0600

    reformat toy_main

--- day6/toy_main.py
+++ day6/toy_main.py
@@ -6,7 +6,6 @@ def orbit_checksum(planet, depth=1):
     global counter_global
     for edge in orbit_map:
         if edge[0] == planet:
-
             counter_global += depth
             orbit_checksum(edge[1], depth + 1)
     return
