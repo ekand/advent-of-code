32525a4d76d11c29e81a8dcd6baddeefead97c2a
Erik Kristofer Anderson <erik.kristofer.anderson@gmail.com>
2019-12-08 22:38:17 -0600

    work on day 6 part b
    But got a new idea for how to proceed. Start from you. step "upstream" and then search downstream until finding santa. I will try to implement this for the next commit.

--- day6/toy_main_b.py
+++ day6/toy_main_b.py
@@ -1,9 +1,31 @@
-with open("input.txt", "r") as file:
+import logging
+
+logging.basicConfig(filename='example.log', level=logging.DEBUG)
+
+
+with open("toy_input_part_b.txt", "r") as file:
     orbit_map = [(string[0:3], string[4:]) for string in file.read().split('\n')]
+
+you_found = False
+san_found = False
+counter_global = 0
 
 
 def orbit_checksum(planet, depth=1):
+    logging.debug(f"checking planet {planet}")
     global counter_global
+    global you_found
+    global san_found
+
+    if planet == 'YOU':
+        you_found = True
+        print("found you")
+    if planet == "SAN":
+        san_found = True
+        print("found santa")
+
+    if you_found and san_found:
+        print("hooray!")
     for edge in orbit_map:
         if edge[0] == planet:
             counter_global += depth
@@ -11,6 +33,6 @@ def orbit_checksum(planet, depth=1):
     return
 
 
-counter_global = 0
-print(orbit_checksum("COM"))
+orbit_checksum("COM")
 print(counter_global)
+
